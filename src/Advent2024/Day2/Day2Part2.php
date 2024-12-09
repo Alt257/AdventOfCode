14f6b53c2b22657dec003de7398e10b3f008e390
Benoît Rataux <benoit.rataux@gmail.com>
2024-12-09 11:02:06 -0500

    #OSEF

--- src/Advent2024/Day2/Day2Part2.php
+++ src/Advent2024/Day2/Day2Part2.php
@@ -26,7 +26,10 @@ class Day2Part2 extends AbstractDay2 {
             '9 7 8 6 5 4 3 2 1',
             '5 7 9 11 13 15 19',
             '5 7 9 11 13 15 15',
-            '5 7 9 11 13 13 19',
+            '5 7 9 11 13 13 19 21',
+            '5 7 9 11 13 13 9 19 21',
+            '5 7 9 11 13 9 19 21',
+            '20 21 20 19 18 17',
         ]);
     }
 
@@ -54,21 +57,21 @@ class Day2Part2 extends AbstractDay2 {
                 if($redNosedSafety > 0) {
                     $redNosedSafety--;
 
-                    // i + 1 don't exist
+                    // i + 1 doesn't exist
                     if(!array_key_exists($i + 1, $report)) {
-                        $solution->setDataStyle(self::CSS_RED_NOSED_DATA, $reportNumber, $i, 'Report');
+                        $solution->setDataStyle(self::CSS_RED_NOSED_DATA, 'Report', $reportNumber, $i);
                         continue;
                     }
-                    // i + 1 exist
+                    // i + 1 exists
                     $i++;
                     $levelDiff = $report[$i] - $report[$i - 2];
                     if($this->isSafe($levelDiff, $min, $max)) {
-                        $solution->setDataStyle(self::CSS_RED_NOSED_DATA, $reportNumber, $i - 1, 'Report');
+                        $solution->setDataStyle(self::CSS_RED_NOSED_DATA, 'Report', $reportNumber, $i - 1);
                         continue;
                     }
 
                     if($i - 2 == 0) {
-                        $solution->setDataStyle(self::CSS_RED_NOSED_DATA, $reportNumber, 0, 'Report');
+                        $solution->setDataStyle(self::CSS_RED_NOSED_DATA, 'Report', $reportNumber, 0);
                         $levelDiff = $report[$i] - $report[$i - 1];
                         if($this->isSafe($levelDiff, $min, $max)) {
                             continue;
@@ -76,7 +79,7 @@ class Day2Part2 extends AbstractDay2 {
                     }
                 }
                 // UNSAFE
-                $solution->setDataStyle(self::CSS_UNSAFE_DATA, $reportNumber, $i, 'Report');
+                $solution->setDataStyle(self::CSS_UNSAFE_DATA, 'Report', $reportNumber, $i);
                 $isSafe = false;
             }
 
@@ -91,6 +94,7 @@ class Day2Part2 extends AbstractDay2 {
             $safeReportsCounter += $isSafe ? 1 : 0;
         }
 
+        var_dump($solution->getDataStyles());
         return $safeReportsCounter;
     }
 
--- src/Entity/Solution.php
+++ src/Entity/Solution.php
@@ -3,14 +3,14 @@
 namespace AOC\Entity;
 
 class Solution {
-
-    private array $dataStyles   = [];
+    
+    private array $dataStyles = [];
     private array $calculations = [];
-    private int   $result;
-
+    private int $result;
+    
     /**
      * @param string $name
-     * @param array  $data
+     * @param array $data
      */
     public function __construct(private string         $name,
                                 private readonly array $data,
@@ -18,14 +18,14 @@ class Solution {
     ) {
         $this->calculationLabel ??= 'Calculation';
     }
-
+    
     /**
      * @return array
      */
     public function getDataStyles(): array {
         return $this->dataStyles;
     }
-
+    
     /**
      * @param array $dataStyles
      *
@@ -35,7 +35,7 @@ class Solution {
         $this->dataStyles = $dataStyles;
         return $this;
     }
-
+    
     public function getDataStyle(string $column,
                                  int    $row,
                                  ?int   $index = null,
@@ -43,61 +43,57 @@ class Solution {
         if($index !== null) return $this->dataStyles[$column][$row][$index] ?? '';
         else                return $this->dataStyles[$column][$row] ?? '';
     }
-
-    public function setDataStyle(array   $styleMap,
-                                 int     $row,
-                                 ?int    $index = null,
-                                 ?string $column = null,
+    
+    public function setDataStyle(array  $styleMap,
+                                 string $column,
+                                 int    $row,
+                                 ?int   $index = null,
     ): self {
         $style = '';
-
+        
         foreach($styleMap as $attribute => $value) {
             $style .= "$attribute: $value; ";
         }
-
-        // @formatter:off
-        if      ($column === null && $index === null)   $this->dataStyles[$row] = $style;
-        else if (                    $index === null)   $this->dataStyles[$column][$row] = $style;
-        else if ($column === null)                      $this->dataStyles[$row][$index] = $style;
-        else                                            $this->dataStyles[$column][$row][$index] = $style;
-        // @formatter:on
-
+        
+        if($index !== null) $this->dataStyles[$column][$row][$index] = $style;
+        else                $this->dataStyles[$column][$row] = $style;
+        
         return $this;
     }
-
+    
     /**
      * @return string|null
      */
     public function getCalculationLabel(): ?string {
         return $this->calculationLabel;
     }
-
+    
     public function setCalculationLabel(string $calculationLabel): self {
         $this->calculationLabel = $calculationLabel;
         return $this;
     }
-
+    
     /**
      * @return string[]
      */
     public function getDataLabels(): array {
         return array_keys($this->data);
     }
-
+    
     /**
      * @return array
      */
     public function getData(): array {
         return $this->data;
     }
-
+    
     /**
      * @return string
      */
     public function getName(): string {
         return $this->name;
     }
-
+    
     /**
      * @param string $name
      *
@@ -107,9 +103,9 @@ class Solution {
         $this->name = $name;
         return $this;
     }
-
+    
     /**
-     * @param int    $row
+     * @param int $row
      * @param string $calculation
      *
      * @return $this
@@ -120,7 +116,7 @@ class Solution {
         $this->calculations[$row] = $calculation;
         return $this;
     }
-
+    
     /**
      * @param int $row
      *
@@ -129,14 +125,14 @@ class Solution {
     public function getCalculation(int $row): string {
         return $this->calculations[$row] ?? '';
     }
-
+    
     /**
      * @return int
      */
     public function getResult(): int {
         return $this->result;
     }
-
+    
     /**
      * @param int $result
      *
@@ -146,5 +142,5 @@ class Solution {
         $this->result = $result;
         return $this;
     }
-
+    
 }
